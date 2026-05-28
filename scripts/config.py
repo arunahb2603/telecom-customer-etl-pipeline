e732b37d9f0da9addba5fe321649ec9465848a1d
arunahb2603 <arunahb2603@gmail.com>
2026-05-28 23:33:22 +0530

    Integrated PostgreSQL database loading into ETL pipeline using Python and Pandas

--- scripts/config.py
+++ scripts/config.py
@@ -1,2 +1,7 @@
 INPUT_PATH = "../data/telecom_churn.csv"
 OUTPUT_PATH = "../output/processed_telecom_churn.csv"
+DB_USER = "postgres"
+DB_PASSWORD = "postgres123"
+DB_HOST = "localhost"
+DB_PORT = 5432
+DB_NAME = "telecom_etl_db"
