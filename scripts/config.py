6db3e88970060ba637e00946e62c993ec2b128ff
arunahb2603 <arunahb2603@gmail.com>
2026-05-29 20:13:44 +0530

    Started AWS S3 cloud integration Setup

--- scripts/config.py
+++ scripts/config.py
@@ -5,3 +5,13 @@ DB_PASSWORD = "postgres123"
 DB_HOST = "localhost"
 DB_PORT = 5432
 DB_NAME = "telecom_etl_db"
+ENVIRONMENT = "local"
+
+from dotenv import load_dotenv
+import os 
+load_dotenv()
+DB_USER = os.getenv("DB_USER")
+DB_PASSOWRD = os.getenv("DB_PASSOWRD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
--- scripts/etl_pipeline.py
+++ scripts/etl_pipeline.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import logging
 import time
-from config import INPUT_PATH, OUTPUT_PATH, DB_USER, DB_PASSWORD,DB_HOST, DB_PORT, DB_NAME
+from config import INPUT_PATH, OUTPUT_PATH, DB_USER, DB_PASSWORD,DB_HOST, DB_PORT, DB_NAME, ENVIRONMENT
 from datetime import datetime
 from sqlalchemy import create_engine
 logging.basicConfig(
@@ -11,7 +11,9 @@ logging.basicConfig(
 )
 engine = create_engine(
     f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+) 
+logging.info(f"Running pipeline in {ENVIRONMENT} environment") 
+
  #read csv file 
   
 try:
@@ -81,16 +83,13 @@ try:
         logging.info(f"Total rows processed: {len(df)}")
         logging.info(f"Total columns processed: {len(df.columns)}")
         logging.info(f"Churn customers: {df['churn'].value_counts().to_dict()}")
+        df = df.drop_duplicates(subset=["customer_id"])
         return df
     #save output 
     def load_data(df):
         logging.info("Saving transformed data...")
         df.to_csv(OUTPUT_PATH,index = False)
-        df.to_sql(
-            name="telecom_customers",
-            con=engine,
-            if_exists="replace",
-            index=False)
+        
     #ETL_DB connectivity and sample output
         query = "SELECT * FROM telecom_customers LIMIT 5"
         sample_df = pd.read_sql (query, engine)
@@ -100,19 +99,31 @@ try:
         count_df = pd.read_sql(row_count_query, engine)
         print(count_df)
         return df
+    def load_to_postgres(df):
+        logging.info("Loading data into PostgreSQL...")
+        df.to_sql(
+            name="telecom_customers",
+            con=engine,
+            if_exists="replace",
+            index=False)
+        logging.info("Data loaded to SQL successfully")
   #-----------main------------------
     def main():
         df=extract_data()
         df=clean_data(df)
         df=transform_data(df)
+        logging.info(df.describe().to_string())
         load_data(df)
+        load_to_postgres(df)
         print("ETL pipeline completed successfully...")
+        
     if __name__ == "__main__":
         print("Script started")
         start_time = time.time()
         main()
         end_time = time.time()
         logging.info(f"Pipeline execution time: {end_time - start_time} seconds")
+        
         
 except Exception as e:
     print(f"Pipeline failed due to error: {e}")
